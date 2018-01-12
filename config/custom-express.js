b4b008549e53dac14ba91fce7488f6c335cd3cd6
Acaciano Neves Souza <acaciano.neves@gmail.com>
2018-01-11 22:41:18 -0200

    criação das validações de pagamentos

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let consign = require('consign');
 let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
 
 module.exports = function () {
     let app = express();
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-
+    app.use(expressValidator()); 
+    
     consign()
         .include('controllers')
         .then('persistencia')
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,12 +6,26 @@ module.exports = function (app) {
 
     app.post("/pagamentos/pagamento", function (req, res) {
         let pagamento = req.body;
-        console.log('processando pagamento...');
+
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatória.").notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal.").notEmpty().isFloat();
+        req.assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3, 3);
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            console.log("Erros de validação encontrados");
+            res.status(400).send(errors);
+            return;
+        }
+
+        pagamento.status = "CRIADO";
+        pagamento.data = new Date;
 
         let connection = app.persistencia.connectionFactory();
         let pagamentoDao = new app.persistencia.pagamentoDao(connection);
 
-        pagamentoDao.salva(pagamento, function (exception, result) {
+        pagamentoDao.salva(pagamento, function (error, result) {
             console.log('pagamento criado: ' + result);
             res.json(pagamento);
         });
